c41676bac867c5eba37e19ac7378988440c8bafb
Cyrilsch3 <Cyril.Schweicher@gmail.com>
2026-02-16 09:28:09 +0100

    Création fichier vaidation2

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,19 +1,16 @@
 import User from '../models/User.js';
 
 export const userService = {
-    // Récupérer uniquement les membres actifs (non archivés)
     async getActiveUsers() {
         return await User.findAll({ where: { isArchived: false } });
     },
 
-    // Logique pour archiver
     async archiveUser(id: string) {
         const user = await User.findByPk(id);
         if (!user) return null;
         return await user.update({ isArchived: true });
     },
 
-    // Logique pour changer le tag avec validation (Point 6.2.4)
     async updateTag(id: string, newTag: string) {
         const user = await User.findByPk(id);
         if (!user) return null;
