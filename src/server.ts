81afbbe6b767cf99f7686d2f785fc9647021a22d
Cyrilsch3 <Cyril.Schweicher@gmail.com>
2026-02-26 19:47:44 +0100

    Persistance des données ajoutées

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 app.use('/api', userRoutes); 
 
-sequelize.sync({ force: true })
+sequelize.sync({ force: false })
     .then(() => {
         console.log('Base de données synchronisée.');
         app.listen(port, () => {
